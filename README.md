7648db9e0be9a4865c80a0af68c010e547cd801c
shafiemukhre <shafiemukhre@gmail.com>
2024-10-12 16:09:46 -0700

    initial

---
+++ README.md
@@ -0,0 +1,75 @@
+
+## Setup
+
+1. Duplicate `.env.sample` file, rename it to `.env` and fill up the values.
+2. `pipenv install -r requirements.txt`
+3. `pipenv shell`
+
+## Running workflow python script (standalone)
+
+- `pipenv run python src/workflow.py` should return correctly output
+
+```
+Based on the provided context, the following LLCs are listed as defendants in the lawsuit:
+
+1. OpenAI, L.L.C.
+2. OpenAI OpCo, LLC
+3. OpenAI Global, LLC
+4. OAI Corporation, LLC
+5. OpenAI Holdings, LLC
+6. Aestas Management Company, LLC
+7. Aestas, LLC
+8. OpenAI Investment LLC
+9. OpenAI Startup Fund Management, LLC
+10. OpenAI Startup Fund GP I, L.L.C.
+11. OpenAI Startup Fund SPV GP I, L.L.C.
+12. OpenAI Startup Fund SPV GP II, L.L.C.
+13. OpenAI Startup Fund SPV GP III, L.L.C.
+14. OpenAI Startup Fund SPV GP IV, L.L.C.
+
+Note that Elon Musk is not mentioned as a plaintiff in the provided context. The lawsuit appears to be filed by OpenAI, L.P. against the listed defendants.
+```
+
+## Running api server locally
+
+```
+$ pipenv run python -m llama_deploy.apiserver
+Loading .env environment variables...
+INFO:     Started server process [55238]
+INFO:     Waiting for application startup.
+INFO:     Application startup complete.
+INFO:     Uvicorn running on http://0.0.0.0:4501
+```
+
+Create a new terminal tab and run 
+
+```
+$ llamactl deploy deployment.yaml
+Deployment successful: QuickStart
+```
+
+```
+$ llamactl run --deployment QuickStart --arg query_str "List all LLCs that Elon Musk is suing"
+Based on the provided context, the LLCs that Elon Musk is suing are:
+
+1. OpenAI Investment LLC
+2. Aestas Management Company, LLC
+3. Aestas, LLC 
+
+These are listed as defendants in the lawsuit.
+```
+
+## Running the api server using docker locally
+
+```
+$ docker run -p 4501:4501 -v .:/opt/quickstart -w /opt/quickstart llamaindex/llama-deploy:main
+INFO:     Started server process [1]
+INFO:     Waiting for application startup.
+INFO:     Application startup complete.
+INFO:     Uvicorn running on http://0.0.0.0:4501
+```
+
+Create a new terminal tab and run
+
+
+
---
+++ src/workflow.py
@@ -0,0 +1,163 @@
+import os
+import asyncio
+from typing import List, Optional
+from llama_index.embeddings.mistralai import MistralAIEmbedding
+from dotenv import load_dotenv
+from llama_index.core.workflow import Workflow, StartEvent, StopEvent, step
+from llama_index.llms.text_generation_inference import (
+    TextGenerationInference,
+)
+from llama_index.core import Settings
+from llama_index.core import PromptTemplate
+from llama_index.vector_stores.pinecone import PineconeVectorStore
+from pinecone import Pinecone, ServerlessSpec
+from llama_index.core.workflow import Event
+from llama_index.core.schema import NodeWithScore
+from llama_index.core.workflow import (
+    Workflow,
+    step,
+    Context,
+    StartEvent,
+    StopEvent,
+)
+from llama_index.core import (
+    VectorStoreIndex,
+    Document,
+    SummaryIndex,
+)
+from llama_index.core.query_pipeline import QueryPipeline
+from llama_index.llms.openai import OpenAI
+from llama_index.core import StorageContext
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.vector_stores.pinecone import PineconeVectorStore
+from IPython.display import Markdown, display
+from llama_index.core.base.base_retriever import BaseRetriever
+
+load_dotenv()
+
+model_name = "mistral-embed"
+
+embed_model = MistralAIEmbedding(model_name=model_name)
+
+Settings.embed_model = embed_model
+
+DEFAULT_RAG_PROMPT = PromptTemplate(
+    template="""Use the provided context to answer the question. If you don't know the answer, say you don't know.
+
+    Context:
+    {context}
+
+    Question:
+    {question}
+    """
+)
+
+
+class PrepEvent(Event):
+    """Prep event (prepares for retrieval)."""
+
+    pass
+
+
+class RetrieveEvent(Event):
+    """Retrieve event (gets retrieved nodes)."""
+
+    retrieved_nodes: list[NodeWithScore]
+
+
+class AugmentGenerateEvent(Event):
+    """Query event. Queries given relevant text and search text."""
+
+    relevant_text: str
+    search_text: str
+
+
+# First things first, we need to create a new class that subclasses Workflow.
+# Each step, now, is a method (decorated by the @step decorator) which will take an Event and Context as input.
+class OpenSourceRAG(Workflow):
+    @step
+    async def prepare_for_retrieval(
+        self, ctx: Context, ev: StartEvent
+    ) -> PrepEvent | None:
+        """Prepare for retrieval."""
+
+        # this is correct, use the HF_LLM_URL
+        model_url = "https://pxtmbr01un274ha7.us-east-1.aws.endpoints.huggingface.cloud"
+
+        query_str: str | None = ev.query_str
+        print("query_str = ", query_str)
+        # query_str: str | None = ev.get("query_str")
+        retriever_kwargs: dict | None = ev.get("retriever_kwargs", {})
+
+        if query_str is None:
+            return None
+
+        pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
+
+        pinecone_index = pc.Index(os.environ["INDEX_NAME"])
+        vector_store = PineconeVectorStore(
+            pinecone_index=pinecone_index,
+        )
+        index = VectorStoreIndex.from_vector_store(vector_store)
+
+        llm = TextGenerationInference(
+            model_url=os.environ["HF_LLM_URL"],
+            token=os.environ["HF_TOKEN"],
+            model_name="hugging-quants/Meta-Llama-3.1-8B-Instruct-AWQ-INT4",
+        )
+        await ctx.set("rag_pipeline", QueryPipeline(chain=[DEFAULT_RAG_PROMPT, llm]))
+
+        await ctx.set("llm", llm)
+        await ctx.set("index", index)
+
+        await ctx.set("query_str", query_str)
+        await ctx.set("retriever_kwargs", retriever_kwargs)
+
+        return PrepEvent()
+
+    @step
+    async def retrieve(self, ctx: Context, ev: PrepEvent) -> RetrieveEvent | None:
+        """Retrieve the relevant nodes for the query."""
+        query_str = await ctx.get("query_str")
+        retriever_kwargs = await ctx.get("retriever_kwargs")
+
+        if query_str is None:
+            return None
+
+        index = await ctx.get("index", default=None)
+        if not (index):
+            raise ValueError(
+                "Index and tavily tool must be constructed. Run with 'documents' and 'tavily_ai_apikey' params first."
+            )
+
+        retriever: BaseRetriever = index.as_retriever(**retriever_kwargs)
+        result = retriever.retrieve(query_str)
+        await ctx.set("query_str", query_str)
+        return RetrieveEvent(retrieved_nodes=result)
+
+    @step
+    async def augment_and_generate(self, ctx: Context, ev: RetrieveEvent) -> StopEvent:
+        """Get result with relevant text."""
+        relevant_nodes = ev.retrieved_nodes
+        relevant_text = "\n".join([node.get_content() for node in relevant_nodes])
+        query_str = await ctx.get("query_str")
+
+        relevancy_pipeline = await ctx.get("rag_pipeline")
+
+        relevancy = relevancy_pipeline.run(context=relevant_text, question=query_str)
+
+        return StopEvent(result=relevancy.message.content)
+
+
+open_source_rag_wf = OpenSourceRAG()
+
+
+async def main():
+    print(
+        # await open_source_rag_wf.run(query_str="What state is the complaint filed in?")
+        await open_source_rag_wf.run(query_str="List all LLCs that Elon Musk is suing")
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
